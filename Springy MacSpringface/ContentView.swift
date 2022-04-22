b19acd6da2b6fbdac2894aa8f6002790b205380b
Enie Weiß <weiss.enie@gmail.com>
2022-04-23 00:29:05 +0200

    Initial Commit

---
+++ Springy MacSpringface/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Springy MacSpringface
+//
+//  Created by Enie Weiß on 23.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Springy MacSpringface/Springy_MacSpringfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Springy_MacSpringfaceApp.swift
+//  Springy MacSpringface
+//
+//  Created by Enie Weiß on 23.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Springy_MacSpringfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
